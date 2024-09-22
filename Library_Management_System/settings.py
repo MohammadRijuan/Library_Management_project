67b8782a8f9bfa6228e50654a8a5c3e01e395480
Rijuan <mohammadrijuan2529@gmail.com>
2024-09-22 11:57:39 +0600

    commit

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-88@1l!=&x2f&^zq@i2w01%(&drk7&^*7&rap#u=66xrx914&2b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
